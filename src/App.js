775bd0d0df5fcbec1d76be971e035a2c17bbf622
SIDRA ZAHID <121977379+sidrazahid@users.noreply.github.com>
2023-12-24 05:54:23 +0500

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginSignup from './Component/LoginSignup/Login.js';
+
+
+function App() {
+  return (
+    <div>
+     <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
